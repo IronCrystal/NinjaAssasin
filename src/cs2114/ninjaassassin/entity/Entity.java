a8d4454320543f6c2c7a1bae6364136dec763084
edf203 <fairhurste@gmail.com>
2014-11-01 19:44:11 -0400

    Entity and Location

--- src/cs2114/ninjaassassin/entity/Entity.java
+++ src/cs2114/ninjaassassin/entity/Entity.java
@@ -1,13 +1,47 @@
 package cs2114.ninjaassassin.entity;
 
+import cs2114.ninjaassassin.world.Location;
+import sofia.graphics.*;
+
 // -------------------------------------------------------------------------
 /**
- *  Class represents entities in the room.
+ * Class represents entities in the room.
  *
- *  @author Andrew Peace
- *  @version Nov 1, 2014
+ * @author Andrew Peace
+ * @author Elliott Fairhurst
+ * @version Nov 1, 2014
  */
 public abstract class Entity
+    extends RectangleShape
 {
+    private Location location;
+    private int      size;
 
+    public Entity(Location loc, String imageName, int size)
+    {
+        this.location = loc;
+        this.size = size;
+
+        setImage(imageName);
+    }
+
+    public void setLocation(Location loc)
+    {
+        this.location = loc;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public Location getLocation()
+    {
+        return location;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
 }
--- src/cs2114/ninjaassassin/world/Location.java
+++ src/cs2114/ninjaassassin/world/Location.java
@@ -2,15 +2,111 @@ package cs2114.ninjaassassin.world;
 
 // -------------------------------------------------------------------------
 /**
- *  Class represents an (x, y) coordinate in a room.
+ * Class represents an (x, y) coordinate in a room with a direction.
  *
- *  @author Andrew Peace
- *  @version Nov 1, 2014
+ * @author Andrew Peace
+ * @author Elliott Fairhurst
+ * @version Nov 1, 2014
  */
 public class Location
 {
-    public void moveRight()
+    private float x;
+    private float y;
+    private float direction;
+
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Location object with a horizontal and vertical position and
+     * a direction.
+     *
+     * @param x
+     *            The horizontal position of the location
+     * @param y
+     *            The vertical position of the location
+     * @param direction
+     *            The direction of the position
+     */
+    public Location(float x, float y, float direction)
     {
-        // Moves to the right
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Sets the horizontal position of the location.
+     *
+     * @param x
+     *            The horizontal position of the location.
+     */
+    public void setX(float x)
+    {
+        this.x = x;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Sets the vertical position of the location.
+     *
+     * @param y
+     *            The vertical position of the location.
+     */
+    public void setY(float y)
+    {
+        this.y = y;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Sets the direction position of the location.
+     *
+     * @param direction
+     *            The direction of the location.
+     */
+    public void setDirection(float direction)
+    {
+        this.direction = direction;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the horizontal position of the location.
+     *
+     * @return The horizontal position of the location.
+     */
+    public float getX()
+    {
+        return x;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the vertical position of the location.
+     *
+     * @return The vertical position of the location.
+     */
+    public float getY()
+    {
+        return y;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the direction of the location.
+     *
+     * @return The direction of the location.
+     */
+    public float getDirection()
+    {
+        return direction;
+    }
+
 }
