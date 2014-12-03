da5840fb6609e31413acb604f90ba1c595ee7202
edf203 <fairhurste@gmail.com>
2014-12-03 14:58:26 -0500

    Fixed while loop in Enemy.update()

--- src/cs2114/ninjaassassin/entity/dynamic/Target.java
+++ src/cs2114/ninjaassassin/entity/dynamic/Target.java
@@ -5,24 +5,30 @@ import cs2114.ninjaassassin.world.Room;
 
 // -------------------------------------------------------------------------
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ * Represents the target character in NinjaAssassin.
  *
- *  @author Andrew Peace (apeace)
- *  @version Dec 3, 2014
+ * @author Andrew Peace (apeace)
+ * @author Elliott Fairhurst (edf203)
+ * @version Dec 3, 2014
  */
-public class Target extends DynamicEntity
+public class Target
+    extends DynamicEntity
 {
 
     // ----------------------------------------------------------
     /**
      * Create a new Target object.
-     * @param loc The Location
-     * @param speed The Speed
-     * @param health The Health
-     * @param lethality The Lethality
-     * @param room The room
+     *
+     * @param loc
+     *            The Location
+     * @param speed
+     *            The Speed
+     * @param health
+     *            The Health
+     * @param lethality
+     *            The Lethality
+     * @param room
+     *            The room
      */
     public Target(
         Location loc,
@@ -34,10 +40,12 @@ public class Target extends DynamicEntity
         super(loc, speed, health, lethality, room);
     }
 
+
     @Override
     public void update()
     {
-        if (this.isCloseTo(getRoom().getNinja())) {
+        if (this.isCloseTo(getRoom().getNinja()))
+        {
             getRoom().setHasWon(true);
         }
 
